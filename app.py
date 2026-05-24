82b728b337357546c34b56f95feb1e64e0570caa
Tania <garciatapiataniaabib@gmail.com>
2026-05-23 22:02:00 -0600

    as

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ pestanas = st.tabs([
 
 with pestanas[0]:
     st.markdown("<h2 style='text-align: center; color: #FF69B4;'>Inicio</h2>", unsafe_allow_html=True)
-    st.text("Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.")
-    st.text("Seleccione **Guardar** para ver o descargar aquí las simulaciones deseadas.")
+    st.text("<h2 style='text-align: center;'>Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.</h2>", unsafe_allow_html=True)
+    st.text("<h2 style='text-align: center;'>Seleccione <strong>Guardar</strong> para ver o descargar aquí las simulaciones deseadas.</h2>", unsafe_allow_html=True)
 
 with pestanas[1]:
     st.markdown("<h2 style='text-align: center; color: #FF69B4;'>Distribución de Bernoulli</h2>", unsafe_allow_html=True)
