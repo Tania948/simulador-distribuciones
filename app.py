c21824ca0e502ea9f142bd34afeecc4d7a82d028
Tania <garciatapiataniaabib@gmail.com>
2026-05-25 08:03:26 -0600

    as

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ pestanas = st.tabs([
 
 with pestanas[0]:
     st.markdown("<h2 style='text-align: center; color: #FF69B4;'>Inicio</h2>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center; font-weight: normal;'>Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.</p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; font-weight: normal;'>Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.</p>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; font-weight: normal;'>Seleccione <strong>Guardar</strong> para ver o descargar aquí las simulaciones deseadas.</p>", unsafe_allow_html=True)
     
 with pestanas[1]:
