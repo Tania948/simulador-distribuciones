4796cfb566aa320c7ec6f2b79eeab34a2d59fabc
Tania <garciatapiataniaabib@gmail.com>
2026-05-25 11:57:09 -0600

    hbj

--- subpaginas/inicio.py
+++ subpaginas/inicio.py
@@ -3,10 +3,6 @@ import streamlit as st
 from css.estilos import titulo_rosa, parrafo_adaptable
 
 def mostrar_inicio():
-    # Usamos la función para el título
     titulo_rosa("Inicio")
-    
-    # Usamos la función para los párrafos
     parrafo_adaptable("Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.")
-    
     parrafo_adaptable("Seleccione <strong>Guardar</strong> para ver o descargar aquí las simulaciones deseadas.")
