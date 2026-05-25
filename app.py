351c4be161f4f08e036fb40814cbf24ba5da6d50
Tania <garciatapiataniaabib@gmail.com>
2026-05-25 08:11:51 -0600

    w

--- app.py
+++ app.py
@@ -36,9 +36,7 @@ pestanas = st.tabs([
 ])
 
 with pestanas[0]:
-    st.markdown("<h2 style='text-align: center; color: #FF69B4;'>Inicio</h2>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: left; font-weight: normal;'>Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.</p>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: left; font-weight: normal;'>Seleccione <strong>Guardar</strong> para ver o descargar aquí las simulaciones deseadas.</p>", unsafe_allow_html=True)
+   st.open("subpaginas/bernoulli.py")
     
 with pestanas[1]:
     st.markdown("<h2 style='text-align: center; color: #FF69B4;'>Distribución de Bernoulli</h2>", unsafe_allow_html=True)
---
+++ subpaginas/bernoulli.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+    st.markdown("<h2 style='text-align: center; color: #FF69B4;'>Inicio</h2>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; font-weight: normal;'>Seleccione la distribución que desea simular en las pestañas superiores. Ajuste el tamaño de la muestra en la barra lateral para ver cómo afecta a las distribuciones.</p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; font-weight: normal;'>Seleccione <strong>Guardar</strong> para ver o descargar aquí las simulaciones deseadas.</p>", unsafe_allow_html=True)
